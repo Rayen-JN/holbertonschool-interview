79a44bff4a6c0eea3e84cc44b8b85f84ad40cc8f
rayen <rayen@kali.kali>
2025-03-01 03:18:33 -1000

    nqueens

---
+++ nqueens/0-nqueens.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+import sys
+
+
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def print_solution(board):
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
+def is_safe(board, row, col):
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solve_nqueens_util(board, col):
+    if col >= len(board):
+        print_solution(board)
+        return True
+
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1) or res
+            board[i][col] = 0
+
+    return res
+
+
+def solve_nqueens(N):
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    if not solve_nqueens_util(board, 0):
+        print("No solution exists")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    N = sys.argv[1]
+
+    if not is_number(N):
+        print("N must be a number")
+        sys.exit(1)
+
+    N = int(N)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve_nqueens(N)
